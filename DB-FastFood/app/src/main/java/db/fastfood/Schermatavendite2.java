e6f55e78220b42cf3086459da2439c1acecfb28e
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-06-10 01:03:08 -0700

    ora è possibile attribuire piu prodotti a un singolo cliente

--- DB-FastFood/app/src/main/java/db/fastfood/Schermatavendite2.java
+++ DB-FastFood/app/src/main/java/db/fastfood/Schermatavendite2.java
@@ -1,12 +1,15 @@
 package db.fastfood;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.Map;
 
 public class Schermatavendite2 extends JFrame {
     private Connection conn;
     private JPanel panel;
+
+    boolean flag = false;
 
     public Schermatavendite2(Connection conn) {
         this.conn = conn;
@@ -107,52 +110,228 @@ public class Schermatavendite2 extends JFrame {
         return idordine;
     }
 
-    private void vendita(String nomeprodotto){
+    private void vendita000000(String nomeprodotto){
         PreparedStatement statement = null;
         ResultSet resultSet = null;
 
-        try{
-            //ottengo ingredienti del prodotto
-            String query = "SELECT P.codice, I.ID, I.quantita, P.descrizione, I.nome_commerciale, PI.quantita_utilizzata  FROM PRODOTTI P JOIN INGREDIENTI_PRODOTTI PI ON P.codice = PI.codice_prodotto JOIN INGREDIENTI I ON PI.ID_ingrediente = I.ID where P.descrizione = ?";
+
+        try {
+
+            int idcliente = getCurrentcliente();
+            int idordine = getCurrentordine();
+            //ottengo il codice del prodotto selezionato
+            int idProdotto = 0;
+            String query = "SELECT codice FROM prodotti WHERE descrizione = ?";
             statement = conn.prepareStatement(query);
             statement.setString(1, nomeprodotto);
             resultSet = statement.executeQuery();
-
             while (resultSet.next()) {
-                int idIngrediente = resultSet.getInt("ID");
-                double quantitaIngrediente = resultSet.getDouble("quantita");
-                double quantitaUtilizzata = resultSet.getDouble("quantita_utilizzata");
-                System.out.println(quantitaUtilizzata);
-                int idProdotto = resultSet.getInt("codice");
-
-                // Calcolo della nuova quantità dell'ingrediente dopo la vendita
-                double nuovaQuantita = quantitaIngrediente - quantitaUtilizzata;
-                System.out.println(nuovaQuantita);
-
-                // Aggiornamento della quantità dell'ingrediente nel database
-                String updateQuery = "UPDATE Ingredienti SET Quantita = ? WHERE ID = ?";
-                PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
-                updateStatement.setDouble(1, nuovaQuantita);
-                updateStatement.setInt(2, idIngrediente);
-                updateStatement.executeUpdate();
-
-
-                //inserisco la vendita nel database del cliente corrente
-
-                int idcliente = getCurrentcliente();
-                int idordine = getCurrentordine();
-                String query2 = "INSERT INTO dettaglio_ordini (ID_ordine, codice_prodotto, quantita) VALUES (?,?,?)";
-                statement = conn.prepareStatement(query2);
-                statement.setInt(1, idordine);
-                statement.setInt(2, idProdotto);
-                statement.setInt(3, 1);
-                statement.executeUpdate();
+                idProdotto = resultSet.getInt("codice");
             }
 
-        }catch (Exception e){
-            e.printStackTrace();
+
+            //verifico se il cliente corrente ha già quel codice prodotto nel suo ordinativo
+            String check = "SELECT codice_prodotto FROM dettaglio_ordini WHERE ID_ordine = ? AND codice_prodotto = ?";
+            PreparedStatement statement22 = conn.prepareStatement(check);
+            statement22.setInt(1, idordine);
+            statement22.setInt(2, idProdotto);
+            ResultSet result2 = statement22.executeQuery();
+
+
+
+            while (result2.next()) {
+                int codice = result2.getInt("codice_prodotto");
+                if (codice == idProdotto) {
+                    //se il cliente ha già quel prodotto nel suo ordine, aggiorno la quantità
+                    String update = "UPDATE dettaglio_ordini SET quantita = quantita + 1 WHERE ID_ordine = ? AND codice_prodotto = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(update);
+                    updateStatement.setInt(1, idordine);
+                    updateStatement.setInt(2, idProdotto);
+                    updateStatement.executeUpdate();
+                }
+            }
+
+
+            while (result2.next()) {
+                int codice = result2.getInt("codice_prodotto");
+                if (codice == idProdotto) {
+                    //se il cliente ha già quel prodotto nel suo ordine, aggiorno la quantità
+                    String update = "UPDATE dettaglio_ordini SET quantita = quantita + 1 WHERE ID_ordine = ? AND codice_prodotto = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(update);
+                    updateStatement.setInt(1, idordine);
+                    updateStatement.setInt(2, idProdotto);
+                    updateStatement.executeUpdate();
+
+                    flag = true;
+                }
+            }
+
+
+            if (flag==false) {
+
+
+
+                //ottengo ingredienti del prodotto
+                String query1 = "SELECT P.codice, I.ID, I.quantita, P.descrizione, I.nome_commerciale, PI.quantita_utilizzata  FROM PRODOTTI P JOIN INGREDIENTI_PRODOTTI PI ON P.codice = PI.codice_prodotto JOIN INGREDIENTI I ON PI.ID_ingrediente = I.ID where P.descrizione = ?";
+                statement = conn.prepareStatement(query1);
+                statement.setString(1, nomeprodotto);
+                resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    int idIngrediente = resultSet.getInt("ID");
+                    double quantitaIngrediente = resultSet.getDouble("quantita");
+                    double quantitaUtilizzata = resultSet.getDouble("quantita_utilizzata");
+                    System.out.println(quantitaUtilizzata);
+                    /*int*/
+                    idProdotto = resultSet.getInt("codice");
+
+                    // Calcolo della nuova quantità dell'ingrediente dopo la vendita
+                    double nuovaQuantita = quantitaIngrediente - quantitaUtilizzata;
+                    System.out.println(nuovaQuantita);
+
+                    // Aggiornamento della quantità dell'ingrediente nel database
+                    String updateQuery = "UPDATE Ingredienti SET Quantita = ? WHERE ID = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+                    updateStatement.setDouble(1, nuovaQuantita);
+                    updateStatement.setInt(2, idIngrediente);
+                    updateStatement.executeUpdate();
+
+
+                    //inserisco la vendita nel database del cliente corrente
+
+                    /*int*/
+                    idcliente = getCurrentcliente();
+                    /*int*/
+                    idordine = getCurrentordine();
+                    String query2 = "INSERT INTO dettaglio_ordini (ID_ordine, codice_prodotto, quantita) VALUES (?,?,?)";
+                    statement = conn.prepareStatement(query2);
+                    statement.setInt(1, idordine);
+                    statement.setInt(2, idProdotto);
+                    statement.setInt(3, 1);
+                    statement.executeUpdate();
+                }
+            }
+
+            }catch(Exception e){
+                e.printStackTrace();
+
         }
+        flag=false;
     }
+
+
+
+
+
+
+    private void vendita(String nomeprodotto) {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        boolean flag = false;
+
+        try {
+            int idcliente = getCurrentcliente();
+            int idordine = getCurrentordine();
+            int idProdotto = 0;
+
+            // Ottieni il codice del prodotto selezionato
+            String query = "SELECT codice FROM prodotti WHERE descrizione = ?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, nomeprodotto);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                idProdotto = resultSet.getInt("codice");
+            }
+
+            // Verifica se il cliente corrente ha già quel codice prodotto nel suo ordinativo
+            String check = "SELECT codice_prodotto FROM dettaglio_ordini WHERE ID_ordine = ? AND codice_prodotto = ?";
+            PreparedStatement statement22 = conn.prepareStatement(check);
+            statement22.setInt(1, idordine);
+            statement22.setInt(2, idProdotto);
+            ResultSet result2 = statement22.executeQuery();
+
+            // Verifica l'esistenza del record nel dettaglio ordine
+            if (result2.next()) {
+                // Il record esiste
+                int codice = result2.getInt("codice_prodotto");
+                if (codice == idProdotto) {
+                    // Se il cliente ha già quel prodotto nel suo ordine, aggiorna la quantità
+                    String update = "UPDATE dettaglio_ordini SET quantita = quantita + 1 WHERE ID_ordine = ? AND codice_prodotto = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(update);
+                    updateStatement.setInt(1, idordine);
+                    updateStatement.setInt(2, idProdotto);
+                    updateStatement.executeUpdate();
+
+                    flag = true;
+                }
+            }
+
+            if (!flag) {
+                // Il prodotto non esiste ancora nel dettaglio ordine, procedi con il resto delle operazioni
+
+                // Ottieni ingredienti del prodotto
+                String query1 = "SELECT P.codice, I.ID, I.quantita, P.descrizione, I.nome_commerciale, PI.quantita_utilizzata  FROM PRODOTTI P JOIN INGREDIENTI_PRODOTTI PI ON P.codice = PI.codice_prodotto JOIN INGREDIENTI I ON PI.ID_ingrediente = I.ID where P.descrizione = ?";
+                statement = conn.prepareStatement(query1);
+                statement.setString(1, nomeprodotto);
+                resultSet = statement.executeQuery();
+
+                while (resultSet.next()) {
+                    int idIngrediente = resultSet.getInt("ID");
+                    double quantitaIngrediente = resultSet.getDouble("quantita");
+                    double quantitaUtilizzata = resultSet.getDouble("quantita_utilizzata");
+                    System.out.println(quantitaUtilizzata);
+                    idProdotto = resultSet.getInt("codice");
+
+                    // Calcolo della nuova quantità dell'ingrediente dopo la vendita
+                    double nuovaQuantita = quantitaIngrediente - quantitaUtilizzata;
+                    System.out.println(nuovaQuantita);
+
+                    // Aggiornamento della quantità dell'ingrediente nel database
+                    String updateQuery = "UPDATE Ingredienti SET Quantita = ? WHERE ID = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+                    updateStatement.setDouble(1, nuovaQuantita);
+                    updateStatement.setInt(2, idIngrediente);
+                    updateStatement.executeUpdate();
+
+                    // Inserisci la vendita nel database del cliente corrente
+                    idcliente = getCurrentcliente();
+                    idordine = getCurrentordine();
+                    String query2 = "INSERT INTO dettaglio_ordini (ID_ordine, codice_prodotto, quantita) VALUES (?,?,?)";
+                    statement = conn.prepareStatement(query2);
+                    statement.setInt(1, idordine);
+                    statement.setInt(2, idProdotto);
+                    statement.setInt(3, 1);
+                    statement.executeUpdate();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Chiudi le risorse correttamente
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        flag = false;
+    }
+
+
+
+
+
+
+
+
+
 
     public void nuovo_cliente() {
         try{
