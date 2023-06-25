ec7cabe78f9cf36d25d182926dadfb9b5c8bbc6c
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-06-25 10:55:18 -0700

    add javadoc

--- DB-FastFood/app/src/main/java/db/fastfood/api/ManagerRichieste.java
+++ DB-FastFood/app/src/main/java/db/fastfood/api/ManagerRichieste.java
@@ -1,7 +1,13 @@
 package db.fastfood.api;
 
 public interface ManagerRichieste {
+    /**
+     * Method that allow insert a new request
+     */
     public void inserisciRichiesta();
 
+    /**
+     * Method that allow to show and decline requests. 
+     */
     public void visualizzaRifiutaRichieste();
 }
