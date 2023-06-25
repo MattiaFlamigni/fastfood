ff6d50e410786f51d97d7f40e119d693b76bbf72
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-06-25 10:57:04 -0700

    javadoc

--- DB-FastFood/app/src/main/java/db/fastfood/api/ManagerIngredienti.java
+++ DB-FastFood/app/src/main/java/db/fastfood/api/ManagerIngredienti.java
@@ -1,10 +1,19 @@
 package db.fastfood.api;
 
 public interface ManagerIngredienti {
+    /**
+     * Method that allow to add ingredients
+     */
     public void aggiungiIngrediente();
 
+    /**
+     * Method that allow to show all ingredients into a product
+     */
     public void visualizzaIngredientiProdotto();
 
+    /**
+     * Method that allow to add an ingredient to a product
+     */
     public void aggiungiIngredienteAProdotto();
     
 }
