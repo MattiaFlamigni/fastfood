41acd922c332eb688148effcb84885fd0566be67
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-06-25 10:55:47 -0700

    javadoc

--- DB-FastFood/app/src/main/java/db/fastfood/api/ManagerProducts.java
+++ DB-FastFood/app/src/main/java/db/fastfood/api/ManagerProducts.java
@@ -1,7 +1,13 @@
 package db.fastfood.api;
 
 public interface ManagerProducts {
+    /**
+     * Method that allow to show all available products
+     */
     public void visualizzaProdottiDisponibili();
 
+    /**
+     * Method that allow to add a new product
+     */
     public void aggiungiProdotto();
 }
