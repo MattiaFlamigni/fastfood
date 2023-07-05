b3d4754e73edb678329954d01dbc6a5026352fe0
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-07-04 23:57:05 -0700

    eliminato warning

--- DB-FastFood/app/src/main/java/db/fastfood/Impl/ManagerImpl.java
+++ DB-FastFood/app/src/main/java/db/fastfood/Impl/ManagerImpl.java
@@ -13,7 +13,6 @@ import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
