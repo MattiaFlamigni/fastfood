a708ea8058e280e87dc77f211ac7917961b29a50
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-07-14 18:34:09 -0700

    aggiunta documentazione

--- DB-FastFood/app/src/main/java/db/fastfood/api/Vendita.java
+++ DB-FastFood/app/src/main/java/db/fastfood/api/Vendita.java
@@ -1,12 +1,29 @@
 package db.fastfood.api;
 
 public interface Vendita {
+
+    /**
+     * to start a new order
+     */
     public void nuovo_cliente();
 
+    /**
+     * 
+     * an order with fidelity card
+     */
     public void inserisci_offerta();
 
+    /**
+     * 
+     * @param nomeprodotto
+     * sold product 
+     *
+     */
     public void vendita(String nomeprodotto);
 
+    /**
+     * an order delivered by app
+     */
     public void delivery();
 
 }
